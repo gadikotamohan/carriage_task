6fc0dcf95dc4c809961430fffe7ace64168d4c87
MOHAN RAJA GADIKOTA <mohan@IMCHLT248.local>
2019-02-15 15:24:49 +0530

    Infinity loop

--- app/serializers/card_details_serializer.rb
+++ app/serializers/card_details_serializer.rb
@@ -2,7 +2,7 @@ class CardDetailsSerializer < ActiveModel::Serializer
   attributes :id, :title, :description
   attribute :comments
   def comments
-    obj_comments = comments.includes(:user).limit(3)
+    obj_comments = self.object.comments.includes(:user).limit(3)
     ActiveModel::Serializer::CollectionSerializer.new(obj_comments, serializer: CommentInfoSerializer, root: false)
   end
 end
